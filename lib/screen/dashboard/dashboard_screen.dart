cf997601ae2a498b1daa771ce0fbc3809c1c7a98
FarighMAkmalullah <48850497+FarighMAkmalullah@users.noreply.github.com>
2023-12-13 20:52:36 +0700

    Pemesanan Carausel selesai

--- lib/screen/dashboard/dashboard_screen.dart
+++ lib/screen/dashboard/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:penyewaan_gedung_triharjo/screen/dashboard/widget/header_widget.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:penyewaan_gedung_triharjo/screen/dashboard/widget/pesan_gedung_widget.dart';
+import 'package:penyewaan_gedung_triharjo/screen/dashboard/widget/profil_gedung_widget.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -10,12 +11,6 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  final _pageController = PageController();
-  List<String> imageUrls = [
-    "assets/image/dashboard/dashboard1.jpg",
-    "assets/image/dashboard/dashboard2.jpg",
-    "assets/image/dashboard/dashboard3.jpg",
-  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -33,79 +28,24 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         bottomLeft: Radius.circular(30),
                       )),
                 ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 25),
-                  child: Column(
-                    children: [
-                      const HeaderWidget(),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Stack(
-                        children: [
-                          Container(
-                            height: 150,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 150,
-                            width: double.infinity,
-                            child: PageView(
-                              controller: _pageController,
-                              children: imageUrls.map(
-                                (imageUrl) {
-                                  return ClipRRect(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    child: Image.asset(
-                                      imageUrl,
-                                      fit: BoxFit.cover,
-                                      height: 150,
-                                    ),
-                                  );
-                                },
-                              ).toList(),
-                            ),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(15),
-                            height: 150,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    const Text(
-                                      "Aplikasi Gedung Triharjo",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                    SmoothPageIndicator(
-                                      controller: _pageController,
-                                      count: imageUrls.length,
-                                      effect: SlideEffect(
-                                        activeDotColor: Colors.white,
-                                        dotHeight: 8,
-                                        dotWidth: 8,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+                const Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: HeaderWidget(),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: ProfilGedungWidget(),
+                    ),
+                    PesanGedungWidget(),
+                  ],
                 ),
               ],
             )
---
+++ lib/screen/dashboard/widget/pesan_carausel_widget.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class PesanCarauselWidget extends StatefulWidget {
+  const PesanCarauselWidget({super.key});
+
+  @override
+  State<PesanCarauselWidget> createState() => _PesanCarauselWidgetState();
+}
+
+class _PesanCarauselWidgetState extends State<PesanCarauselWidget> {
+  Map<String, dynamic> carouselList = {
+    'carousel': [
+      {
+        'title': 'Aula Balai Kelurahan',
+        'image': 'assets/image/dashboard/aula.png',
+      },
+      {
+        'title': 'Gedung Olahraga',
+        'image': 'assets/image/dashboard/gedung.png',
+      },
+      {
+        'title': 'Lapangan Olahraga',
+        'image': 'assets/image/dashboard/lapangan.png',
+      },
+    ]
+  };
+  @override
+  Widget build(BuildContext context) {
+    var list = carouselList?['carousel'];
+    return Container(
+      height: 220,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: list?.length ?? 0,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.only(
+              left: 16,
+              right: index == list?.length - 1 ? 16 : 0,
+              top: 5,
+              bottom: 5,
+            ),
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('${list?[index]['image']}'),
+                  fit: BoxFit.fill),
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0, 0),
+                  blurRadius: 4,
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+            width: 190,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  list?[index]['title'] ?? '',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 1.0,
+                  child: SizedBox(
+                    height: 30,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 0,
+                        ),
+                        child: const Text(
+                          'Pesan Sekarang',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        )),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
